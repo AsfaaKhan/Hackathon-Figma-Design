977965c6244e54a84add74f78f9ca34b5c529ad3
AsfaaKhan <khanzadiasfi0314@gmail.com>
2024-12-09 02:09:06 +0500

    Figma Design

--- components/submitbtn.tsx
+++ components/submitbtn.tsx
@@ -1,12 +1,13 @@
 
 
+interface ButtonProps { name: string; }
 
-const SubmitButton =(props: any)=>{
+const SubmitButton =(props: ButtonProps)=>{
     return(
     
             <section>
                 <div className=" rounded-[37px] border  border-skyblue  w-[184px] h-[52px] pt-[15px] pb-[15px] pr-[36px] pl-[36px] gap-[10px] text-skyblue font-bold text-sm leading-[24px] text-center items-center "  style={{letterSpacing:"0.2px"}}>{props.name}
-                <h2 >{props.btn}</h2>
+                
                 </div>
                 
             </section>
